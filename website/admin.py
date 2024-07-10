24d0925ac955dd89b263d81a928c5afd9c09237f
Liliana Bassa <lilianabassa0510@gmail.com>
2024-07-10 17:47:16 +0200

    Add Artist, Art and Category to admin section

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category, Art, Artist
 
-# Register your models here.
+admin.site.register(Artist)
+admin.site.register(Category)
+admin.site.register(Art)
+
